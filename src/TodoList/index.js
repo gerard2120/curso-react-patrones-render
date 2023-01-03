a4fc0198d1136b1f12425e7de8492c9f3ea6f215
geradr2121 <luis.gerard2120@gmail.com>
2023-01-03 10:39:43 -0600

    add composition components, and render props

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -3,9 +3,16 @@ import './TodoList.css'
 
 function TodoList(props) {
   return (
-    <section>
+    <section className='TodoList-container'>
       <ul>
-        {props.children}
+        {props.error && props.onError()}
+        {props.loading && props.onLoading()}
+        {(!props.loading && !props.searchedTodos.length) && props.onEmptyTodos()}
+        
+        {/* dos formas derentes de renderizar el todo que estamos iterando una mandando la funcion y otra la propiedad
+        ya que esta iterando de las dos maneras las propiedades
+        {props.searchedTodos.map(todo => props.render(todo))} */}
+        {props.searchedTodos.map(props.render)}
       </ul>
     </section>
   );
